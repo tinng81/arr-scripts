a660c655c234bc12aff1ff3cba08b04c42a59c41
Tin Nguyen <tinng801@gmail.com>
2023-07-26 22:44:18 -0300

    FIX yt-dlp older version, use latest from pip3

--- sonarr/setup.bash
+++ sonarr/setup.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/with-contenv bash
-SMA_PATH="/usr/local/sma"
 
 echo "************ install packages ************" && \
-apk add -U --update --no-cache \
+apt-get -qqy update && \
+apt-get -qqy install \
+    libxml2-utils
 	flac \
 	opus-tools \
 	jq \
@@ -11,93 +12,43 @@ apk add -U --update --no-cache \
 	mkvtoolnix \
 	python3-dev \
 	libc-dev \
-	py3-pip \
+	python3-pip \
 	gcc \
-	ffmpeg \
-	yt-dlp && \
+	ffmpeg
+
 echo "************ install python packages ************" && \
-pip install --upgrade --no-cache-dir -U \
+pip3 install --upgrade --no-cache-dir -U \
 	excludarr \
-	yq && \
-echo "************ setup SMA ************" && \
-echo "************ setup directory ************" && \
-mkdir -p ${SMA_PATH} && \
-echo "************ download repo ************" && \
-git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git ${SMA_PATH} && \
-mkdir -p ${SMA_PATH}/config && \
-echo "************ create logging file ************" && \
-mkdir -p ${SMA_PATH}/config && \
-touch ${SMA_PATH}/config/sma.log && \
-chgrp users ${SMA_PATH}/config/sma.log && \
-chmod g+w ${SMA_PATH}/config/sma.log && \
-echo "************ install pip dependencies ************" && \
-python3 -m pip install --user --upgrade pip && \	
-pip3 install -r ${SMA_PATH}/setup/requirements.txt && \
-echo "************ install recyclarr ************" && \
-mkdir -p /recyclarr && \
-wget "https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-musl-x64.tar.xz" -O "/recyclarr/recyclarr.tar.xz" && \
-tar -xf /recyclarr/recyclarr.tar.xz -C /recyclarr &>/dev/null && \
-chmod 777 /recyclarr/recyclarr
-apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/community dotnet7-runtime
+	xq \
+  yt-dlp \
+	yq
 
 mkdir -p /custom-services.d
-echo "Download QueueCleaner service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/universal/services/QueueCleaner -o /custom-services.d/QueueCleaner
-echo "Done"
-
-echo "Download AutoConfig service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/AutoConfig.service -o /custom-services.d/AutoConfig
-echo "Done"
-
 echo "Download AutoExtras service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/AutoExtras.service -o /custom-services.d/AutoExtras
-echo "Done"
-
-echo "Download InvalidSeriesAutoCleaner service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/InvalidSeriesAutoCleaner.service -o /custom-services.d/InvalidSeriesAutoCleaner
-echo "Done"
-
-echo "Download Recyclarr service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/universal/services/Recyclarr -o /custom-services.d/Recyclarr
+curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/sonarr/AutoExtras.service -o /custom-services.d/AutoExtras &>/dev/null
 echo "Done"
 
 echo "Download YoutubeSeriesDownloader service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/YoutubeSeriesDownloader.service -o /custom-services.d/YoutubeSeriesDownloader
+curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/sonarr/YoutubeSeriesDownloader.service -o /custom-services.d/YoutubeSeriesDownloader &>/dev/null
 echo "Done"
 
 mkdir -p /config/extended
 echo "Download Naming script..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/naming.json -o /config/extended/naming.json 
+curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/sonarr/naming.json -o /config/extended/naming.json &>/dev/null
 echo "Done"
 
 mkdir -p /config/extended
 echo "Download Script Functions..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/universal/functions.bash -o /config/extended/functions
-echo "Done"
-
-echo "Download PlexNotify script..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/PlexNotify.bash -o /config/extended/PlexNotify.bash 
-echo "Done"
-
-echo "Download DailySeriesEpisodeTrimmer script..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/DailySeriesEpisodeTrimmer.bash -o /config/extended/DailySeriesEpisodeTrimmer.bash 
+curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/universal/functions.bash -o /config/extended/functions &>/dev/null
 echo "Done"
 
 echo "Download Extras script..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/Extras.bash -o /config/extended/Extras.bash 
-echo "Done"
-
-echo "Download SMA config..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/sma.ini -o /config/extended/sma.ini 
-echo "Done"
-
-echo "Download Recyclarr config..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/recyclarr.yaml -o /config/extended/recyclarr.yaml
+curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/sonarr/Extras.bash -o /config/extended/Extras.bash  &>/dev/null
 echo "Done"
 
 if [ ! -f /config/extended.conf ]; then
 	echo "Download Extended config..."
-	curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/extended.conf -o /config/extended.conf
+	curl https://raw.githubusercontent.com/tinng81/arr-scripts/main/sonarr/extended.conf -o /config/extended.conf &>/dev/null
 	chmod 777 /config/extended.conf
 	echo "Done"
 fi
